0ca518fa8036091a1f301e1789239d133f4acd24
bryanekong <thebryanekong@gmail.com>
2021-06-17 08:45:51 -0700

    script that prints a messge without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main( int argc, char** argv )
+int main(void)
 {
-write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 13);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
 return (0);
 }
