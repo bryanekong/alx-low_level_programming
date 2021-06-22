e5256e796c86ce976764ece145f9d0cd9abcbcc1
bryanekong <thebryanekong@gmail.com>
2021-06-22 13:00:23 -0700

    program that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,18 @@
 
 /**
  * print_last_digit - check the code for ALX School students.
- * n@: is the int that will use for the argument of the function
- * Return: interger value.
+ * @n: is the int that will use for the argument of the function
+ * Return: interger value
  */
 int print_last_digit(int n)
 {
-int last = n % 10;
+int last;
 
-if (n < 0)
-{
+last = n % 10;
+if (last < 0)
 last = last * -1;
-}
+
 _putchar(last + '0');
+
 return (last);
 }
