e20f90fbef0ae6de3a2470d05854c087453aa04d
bryanekong <thebryanekong@gmail.com>
2021-06-22 08:02:47 -0700

    script to check for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _islower - a function that checks for lowercase chatacter
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
