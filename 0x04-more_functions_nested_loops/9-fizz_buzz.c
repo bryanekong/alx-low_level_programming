e95e3654a4cecddddf2f46b75588187c3c8bbc60
bryanekong <thebryanekong@gmail.com>
2021-06-24 09:25:33 -0700

    program that prints fizz buzz in a pattern

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,21 @@
 int main(void)
 {
 int i;
-for (i=1; i<=100; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i%15 == 0)       
-printf ("FizzBuzz\t");   
-         
-else if ((i%3) == 0)   
-printf("Fizz\t");                
-       
-else if ((i%5) == 0)                      
-printf("Buzz\t");                
-     
+if (i % 15 == 0)
+printf("FizzBuzz\t");
+
+else if ((i % 3) == 0)
+printf("Fizz\t");
+
+else if ((i % 5) == 0)
+printf("Buzz\t");
+
 else
-printf("%d\t", i);                
- 
+printf("%d\t", i);
+
 }
- 
+
 return (0);
 }
