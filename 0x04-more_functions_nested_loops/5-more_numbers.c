5e1e636fa6ddcb35c64f83758fa1208f12373c3f
bryanekong <thebryanekong@gmail.com>
2021-06-24 04:22:05 -0700

    program that prints the numbers from 0-9 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * more_numbers_x10 - print the numbers from 0-9 ten times
+ */
+void more_numbers(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
