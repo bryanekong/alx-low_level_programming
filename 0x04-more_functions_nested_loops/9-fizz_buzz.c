ea12368b4575881c07ee708967f4e1aca3f02551
bryanekong <thebryanekong@gmail.com>
2021-06-24 09:48:32 -0700

    print fizzbuzz pattern

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * main - entry block
+ * @void: no argument
+ * Return: 0 Success
  */
-
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
-{
+
+for (i = 1; i < 100; i++)
 if (i % 15 == 0)
-printf("FizzBuzz\t");
-
-else if ((i % 3) == 0)
-printf("Fizz\t");
-
-else if ((i % 5) == 0)
-printf("Buzz\t");
-
-else
-printf("%d\t", i);
-
+{
+printf("FizzBuzz ");
 }
-
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", i);
+}
+printf("Buzz");
+printf("\n");
 return (0);
 }
