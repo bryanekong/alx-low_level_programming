c312ba8b3e2a343b82a14ea792a8083fe6b639d1
bryanekong <thebryanekong@gmail.com>
2021-06-24 04:37:43 -0700

    program that prints numbers 0-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,11 @@ char c;
 
 for (i = 0; i < 10; i++)
 {
-for (c = '0'; c <= '14'; c++)
+for (c = 0; c <= 14; c++)
 {
-_putchar(c);
+if (c > 9)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
 _putchar('\n');
 }
