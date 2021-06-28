616af270c6954d1d0636c3e8a1135224933984c5
bryanekong <thebryanekong@gmail.com>
2021-06-28 10:02:55 -0700

    program that swaps value of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton"
+
+/**
+ *swap_int - a function that swaps 2 integers
+ *@a: first integer
+ *@b: second integer
+ *Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
