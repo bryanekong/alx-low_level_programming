963955bf10be0926b9e8b698b7ba2e2507b0fcf6
bryanekong <thebryanekong@gmail.com>
2021-07-07 07:03:04 -0700

    a string that displays the lenth of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,12 @@
 /**
  * _strlen_recursion - a function that returns the length of a string
  * @s: the string
+ * Return: a number that displays the length of the string
  */
 
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-return (0);
 return (1 + _strlen_recursion(s + 1));
+return (0);
 }
