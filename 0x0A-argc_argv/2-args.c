6b851ce0c059bbbe9c9c6f870e28b1d94f9b1d3e
bryanekong <thebryanekong@gmail.com>
2021-07-12 09:42:24 -0700

    a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 #include "holberton.h"
 
 /**
- * main - a program that prints the number of arguments passed into it
+ * main - a program that prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 always
