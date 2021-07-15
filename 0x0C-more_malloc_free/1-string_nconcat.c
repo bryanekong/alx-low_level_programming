8ee8f0676ce3d4a6a8b6e510ad9fd1fad8d1b0c7
bryanekong <thebryanekong@gmail.com>
2021-07-15 09:57:23 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,38 +11,35 @@
  * Return: newly allocated space in memory;
  * NULL if the function fails
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ar;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int m;
-	unsigned int p;
-	unsigned int k = 0;
-	unsigned int len;
+unsigned int i, j, k;
+char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (j > n)
-		j = n;
-
-	len = i + j;
-
-	ar = malloc(sizeof(char) * (len + 1));
-	if (ar == NULL)
-		return (NULL);
-
-	for (p = 0; p < i; p++)
-		ar[k++] = s1[p];
-	for (m = 0; m < j; m++)
-		ar[k++] = s2[m];
-
-	ar[k] = '\0';
-	return (ar);
+if (s1 == NULL)
+i = 0;
+else
+{
+for (i = 0; s1[i]; i++)
+;
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+if (j > n)
+j = n;
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
 }
