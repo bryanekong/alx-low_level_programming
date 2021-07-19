099d2694fa573a5c1da65414673a84925cd082d8
bryanekong <thebryanekong@gmail.com>
2021-07-19 11:12:49 -0700

    test again

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+#define ABS(x) ((x < 0) ? -x : x)
 #endif
