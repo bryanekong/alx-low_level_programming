bfa8e5209665253c099fcd49d78b6c4b8778a8e2
bryanekong <thebryanekong@gmail.com>
2021-07-19 12:14:27 -0700

    cute poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - describes the struct of dog
+ * @name: name of the dog
+ * @age: how old is the dog?
+ * @owner: who owns the dog?
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
